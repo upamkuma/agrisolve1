caf18e2606688827a34ae47c21a3aeeb55e98358
Pranjal Upadhyay <pranjalupadhyay13@gmail.com>
2024-09-04 21:57:14 +0530

    configed firebase with express and making a dummy contract call

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,15 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const express=require('express')
 const cors=require('cors')
+const admin = require('firebase-admin')
+
+const serviceAccount = require('./agrisolve-7-firebase-adminsdk-n2sgb-da9e5cd090.json')
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+})
+
+const db = admin.firestore();
 
 const app= express();
 
@@ -10,7 +19,14 @@ app.use(cors({
     origin:"http://localhost:5173"
 }))
 app.use(express.json())
-app.get('/test', function(req, res){
-    res.json("test ok")
+app.get('/test', async(req, res) => {
+    try{
+        const contractsSnapshot = await db.collection('contracts').get();
+        const contracts = contractsSnapshot.docs.map(doc => doc.data());
+        res.json(contracts); 
+    } catch (error){
+        console.error("Error fetching contracts:", error);
+        res.status(500).json({error: "Failed to fetch contracts"});
+    }
 })
 app.listen(4000)
