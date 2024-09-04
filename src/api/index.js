c9019793632a2a4b7c654d61a1280fc00b3bf977
aman68695613 <amansrivastav6869@gmail.com>
2024-09-04 20:51:50 +0530

    implement test server in express

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const express=require('express')
+const cors=require('cors')
+
+const app= express();
+
+app.use(cors({
+    credentials: true,
+    origin:"http://localhost:5173"
+}))
+app.use(express.json())
+app.get('/test', function(req, res){
+    res.json("test ok")
+})
+app.listen(4000)
